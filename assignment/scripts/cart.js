4fd6d41677b9311901606243aa17d22a9aa48e34
Davey-M <davey.meuer@gmail.com>
2022-01-07 13:19:50 -0600

    began creating basket class

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+class Basket {
+    constructor() {
+        this.id = Math.floor(Math.random() * 899_999) + 100_000;
+        this.basket = [];
+        this.maxItems = 5;
+    }
+
+    addItem(item) {
+        this.basket.push(item);
+    }
+
+    listItems() {
+        for (let item of this.basket) {
+            console.log(`Basket ${this.id}, Item: ${indexOf(item)}: ${item}`)
+        }
+    }
+}
